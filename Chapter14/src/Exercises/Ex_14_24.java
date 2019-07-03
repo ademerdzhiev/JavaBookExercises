48b3aae2cec72e1399912052eee6aed0c777cca3
angel <angel@aspireV13.angel.home.com>
2019-07-03 07:38:11 +0300

    morning commit

--- Chapter14/src/Exercises/Ex_14_24.java
+++ Chapter14/src/Exercises/Ex_14_24.java
@@ -3,6 +3,10 @@ package Exercises;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Polygon;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 import java.util.ArrayList;
@@ -15,7 +19,28 @@ public class Ex_14_24 extends Application {
         Pane pane = new Pane();
         Scene scene = new Scene(pane, 300, 450);
 
+        List<Double> points = enterPoints();
+        Polygon polygon = new Polygon(points.get(0), points.get(1),
+                points.get(2), points.get(3),
+                points.get(4), points.get(5),
+                points.get(6), points.get(7));
 
+        polygon.setFill(Color.TRANSPARENT);
+        polygon.setStroke(Color.BLACK);
+
+        Text text = new Text();
+        text.setX(20);
+        text.setY(300);
+
+        if (checkPoint(polygon, points.get(8), points.get(9))) {
+            text.setText("point inside polygon");
+        } else {
+            text.setText("point outside polygon");
+        }
+
+        Circle circle = new Circle(points.get(8), points.get(9), 15);
+
+        pane.getChildren().addAll(polygon, circle, text);
 
         stage.setScene(scene);
         stage.setTitle("INSIDE A POLYGON");
@@ -44,4 +69,9 @@ public class Ex_14_24 extends Application {
 
         return points;
     }
+
+    private static boolean checkPoint(Polygon polygon, double x, double y) {
+        return polygon.contains(x, y);
+    }
+
 }
--- Chapter14/src/Exercises/Ex_14_25.java
+++ Chapter14/src/Exercises/Ex_14_25.java
@@ -1,4 +1,56 @@
 package Exercises;
 
-public class Ex_14_25 {
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import javafx.stage.Stage;
+import javafx.util.Pair;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class Ex_14_25 extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+        Pane pane = new Pane();
+
+        double paneWidth = 450;
+        double paneHeight = 300;
+
+        Circle circle = new Circle(paneWidth/2, paneHeight/2, 100);
+        circle.setFill(Color.TRANSPARENT);
+        circle.setStroke(Color.BLUE);
+
+        pane.getChildren().addAll(circle);
+
+        Scene scene = new Scene(pane, 450, 300);
+        stage.setScene(scene);
+        stage.setTitle("Random Points on a circle");
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+
+    private static List<Pair<Double, Double>> generateRandomPoints(double maxX, double maxY, double radius) {
+        List<Pair<Double, Double>> list = new ArrayList<>();
+
+        for (int i = 0; i < 5; i++) {
+            while (true) {
+                double x = (double) (Math.random()*maxX - (maxX - radius) + (maxX - radius));
+                double y = (double) (Math.random()*maxY - (maxY - radius) + (maxY - radius));
+
+
+
+                if()
+
+            }
+        }
+
+        return list;
+    }
 }
